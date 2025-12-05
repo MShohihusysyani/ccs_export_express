1984b8cd3e363427c8691cb41cc3c42e9bfb1cf5
Shohi <shohimatsu11@gmail.com>
2025-12-05 13:31:07 +0700

    feat : tambah alias port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const exportRoute = require('./src/routes/export');
 const cors = require('cors');
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
